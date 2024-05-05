05073d6f44730ee1a43bc149851ef7a56f5194f3
Jonathan Forhan <jonathan.forhan@gmail.com>
2024-05-05 12:31:10 -0400

    two sum

---
+++ arrays_and_hashing/two_sum.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/two-sum/
+
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        seen = {}
+
+        for i, n in enumerate(nums):
+            if seen[n]:
+                return [seen[n], i]
+            else:
+                diff = target - n
+                seen[diff] = i
+
+        raise Exception("assumed to have valid answer")
