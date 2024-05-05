1c03ce74e2d6476271d96041b203b025b1dd9649
Jonathan Forhan <jonathan.forhan@gmail.com>
2024-05-05 12:07:50 -0400

    valid anagram

---
+++ arrays_and_hashing/valid_anagram.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/valid-anagram/
+
+
+from collections import defaultdict
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d = defaultdict(lambda: 0)
+
+        for c in s:
+            d[c] += 1
+
+        for c in t:
+            d[c] -= 1
+
+        for v in d.values():
+            if v != 0:
+                return False
+
+        return True
